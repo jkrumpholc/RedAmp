f8441aea510d5a822aabb7fb7f5aafb1ec5fa845
Mundik <jankrumpholc99@gmail.com>
2023-08-04 12:24:22 +0200

    Argument parsing

---
+++ src/Logger.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def err_handler(error_text: str, help_text: str = None) -> None:
+    """
+    Error handler
+    :param error_text: Error text
+    :type error_text: str
+    :param help_text: Explanation text
+    :type help_text: str
+    :return: None
+    """
+    sys.stderr.write(error_text+"\n")
+    sys.stderr.flush()
+    if help_text is not None:
+        sys.stdout.write(help_text+"\n")
+        sys.stdout.flush()
+    input("Press Return to exit: ")
+    exit(-1)
---
+++ src/main.py
@@ -0,0 +1,23 @@
+import argparse
+import src.Logger as Logger
+
+
+def arg_parsing() -> argparse.Namespace:
+    """
+    :return: Arguments
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--help", action='store_true', help="Show help.")
+    parser.add_argument("--file", help="Source file with links to data sources.")  # argument for file with links
+    parse_args = parser.parse_args()
+    if parse_args.help:
+        pass
+    if not parse_args.file:
+        Logger.err_handler("No input data","Please set argument '--file' or 'dir'. For info use '--help'")
+    return parse_args
+
+
+if __name__ == '__main__':
+    args = arg_parsing()
+    
