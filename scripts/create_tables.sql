a310eb1a691da9753d580015404d7748072b6861
Mundik <jankrumpholc99@gmail.com>
2023-08-04 19:18:55 +0200

    Added missing SQL scripts

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -15,3 +15,24 @@ create table public.url_ioc
     source varchar(255) not null,
     data   varchar(255) not null
 );
+
+create table public.sources
+(
+    url        varchar not null,
+    ioc_type   varchar not null,
+    from_index integer not null,
+    to_index   integer not null
+);
+
+create sequence public.ip_ioc_id_seq
+    as integer;
+alter sequence public.ip_ioc_id_seq owned by public.ip_ioc.id;
+
+create sequence public.url_ioc_id_seq
+    as integer;
+alter sequence public.url_ioc_id_seq owned by public.url_ioc.id;
+
+
+
+
+
