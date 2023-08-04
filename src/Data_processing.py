e3db519074bd25ed07767f6931f8351b5cafd798
Mundik <jankrumpholc99@gmail.com>
2023-08-04 14:07:07 +0200

    Link parsing & data collector

---
+++ src/Data_processing.py
@@ -0,0 +1,22 @@
+import os
+import src.Logger as Logger
+import requests
+
+
+def parse_links(file_name) -> bool:
+    """
+    :param file_name: Name of file with links
+    :type file_name: str
+    :rtype: bool
+    """
+    if not os.path.exists(file_name):   # check if file exists
+        Logger.err_handler("Target doesn't exists", f"Target file: {file_name} does not exists.")
+    if not os.path.isfile(file_name):   # check if target is a file
+        Logger.err_handler("Target is not a file")
+    with open(file_name, "r+") as file:
+        url = file.readline().strip()
+        req = requests.get(url)
+        resp = req.content.decode()     # decode response to get file content
+    return True
+
+
